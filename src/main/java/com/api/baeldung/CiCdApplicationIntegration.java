cf225b89fa8b36606632811fb1d18a90731bb000
Cornachione <leonardo.cornachione@dxc.com>
2021-01-27 17:24:51 -0300

    CodeCov for - Highest code quality

--- src/main/java/com/api/baeldung/CiCdApplicationIntegration.java
+++ src/main/java/com/api/baeldung/CiCdApplicationIntegration.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaeldungCiCdProcessApplication {
+public class CiCdApplicationIntegration {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaeldungCiCdProcessApplication.class, args);
+		SpringApplication.run(CiCdApplicationIntegration.class, args);
 	}
 
 }
